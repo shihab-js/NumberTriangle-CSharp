adac495731811c8f4c9bfb058650b3f498f160dd
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 16:42:04 +0600

    program completed....

--- NumberTriangle/Program.cs
+++ NumberTriangle/Program.cs
@@ -12,6 +12,26 @@ namespace NumberTriangle
             //print show message
             Console.Write("Please enter the range: ");
 
+            //catch the user input
+            n = int.Parse(Console.ReadLine());
+
+            //for loop to continue the program
+            for(i=1; i <= n; i++)
+            {
+                for (j = 0; j < n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(k = 1; k <= i; k++)
+                {
+                    Console.Write(k);
+                }
+                for(l = i-1; l >= 1; l--)
+                {
+                    Console.Write(l);
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
