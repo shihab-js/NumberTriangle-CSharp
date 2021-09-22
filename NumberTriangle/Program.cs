09bd405ac716dc74c5d4b026f2c4297f2ab72ed8
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 16:23:33 +0600

    variables has been declared .....

--- NumberTriangle/Program.cs
+++ NumberTriangle/Program.cs
@@ -6,7 +6,12 @@ namespace NumberTriangle
     {
         static void Main(string[] args)
         {
-            
+            //declare the variables
+            int i, j, k, l, n;
+
+            //print show message
+            Console.Write("Please enter the range: ");
+
         }
     }
 }
